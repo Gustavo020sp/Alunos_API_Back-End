c5282bae26e03fe10f5446f8db394c8373eb05e2
Gustavo Gomes <gustavo020sp@gmail.com>
2024-09-11 10:05:17 -0300

    code adjustments

--- Aluno_API/Program.cs
+++ Aluno_API/Program.cs
@@ -11,9 +11,9 @@ namespace Aluno_API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddScoped<IAlunosService, AlunosService>();
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
