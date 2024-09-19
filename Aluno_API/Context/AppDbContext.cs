4e5e09aec39b7699d01b93862a71b2a69cada1e1
Gustavo Gomes <gustavo020sp@outlook.com>
2024-09-19 20:00:41 -0300

    delete migrations

--- Aluno_API/Context/AppDbContext.cs
+++ Aluno_API/Context/AppDbContext.cs
@@ -11,32 +11,32 @@ namespace Aluno_API.Context
         public DbSet<Aluno> Alunos { get; set; }
 
         //seed database
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Aluno>().HasData(
-        //        new Aluno
-        //        {
-        //            Id = 1,
-        //            Nome = "Antonio José",
-        //            Email = "antonio@gmail.com",
-        //            Idade = 31
-        //        },
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Antonio José",
+                    Email = "antonio@gmail.com",
+                    Idade = 31
+                },
 
-        //        new Aluno
-        //        {
-        //            Id = 2,
-        //            Nome = "Maria Penha",
-        //            Email = "maria@gmail.com",
-        //            Idade = 39
-        //        },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Maria Penha",
+                    Email = "maria@gmail.com",
+                    Idade = 39
+                },
 
-        //        new Aluno
-        //        {
-        //            Id = 3,
-        //            Nome = "Jonas Gomes",
-        //            Email = "jonas@gmail.com",
-        //            Idade = 20
-        //        });
-        //}
+                new Aluno
+                {
+                    Id = 3,
+                    Nome = "Jonas Gomes",
+                    Email = "jonas@gmail.com",
+                    Idade = 20
+                });
+        }
     }
 }
